0b8bae6a8e35116dd34e965ec690d8a314324e7b
sadeer66 <65467413+sadeer66@users.noreply.github.com>
2026-09-19 22:40:11 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "furniture-planner-v30-furniture-schedule-visible-pdf-rights";
+const CACHE_NAME = "furniture-planner-v31-visible-controls-guide";
 const CRITICAL = ['./', './index.html'];
 
 self.addEventListener('install', event => {
