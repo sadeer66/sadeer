c0c779d53e774f1acc6d708a7929004420bf5f3d
sadeer66 <65467413+sadeer66@users.noreply.github.com>
2026-09-20 18:26:33 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "furniture-planner-v39-toolbar-watchdog";
+const CACHE_NAME = "taatheethi-v36-photoshop-icons";
 const CRITICAL = ['./', './index.html'];
 
 self.addEventListener('install', event => {
