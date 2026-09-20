2903a5ffe5ab94668d5b58eeeddaa0f3e743f6e2
sadeer66 <65467413+sadeer66@users.noreply.github.com>
2026-09-20 19:11:53 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "taatheethi-v39-unified-topbar-lefttoolbar";
+const CACHE_NAME = "taatheethi-v40-final-bars-openfix";
 const CRITICAL = ['./', './index.html'];
 
 self.addEventListener('install', event => {
