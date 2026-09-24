4d56f759411cc277a0b94428329b52b2a0be39f9
sadeer66 <65467413+sadeer66@users.noreply.github.com>
2026-09-24 23:27:12 +0300

    Add Wasoolat Electron desktop launcher

---
+++ wasoolat-desktop/main.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+
+app.setName('برنامج الوصولات');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 820,
+    minWidth: 900,
+    minHeight: 620,
+    show: false,
+    autoHideMenuBar: true,
+    backgroundColor: '#eef7f9',
+    icon: path.join(__dirname, 'app', 'apple-touch-icon.png'),
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'app', 'index.html'));
+
+  win.once('ready-to-show', () => {
+    win.maximize();
+    win.show();
+  });
+
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    if (/^https?:/i.test(url)) shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
